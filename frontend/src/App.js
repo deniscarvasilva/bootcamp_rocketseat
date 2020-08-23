cf77c4e94c949ab5486ce04759bcd632759bbb91
deniscarvasilva <deniscarvasilva@gmail.com>
2020-08-23 12:18:04 -0300

    finalização de conceitos do react JS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
+import './App.css';
+import api from './services/api';
+
 function App() {
 
-    const projects = ['Desenvolvimento de app', 'Front-end web'];
+    const [projects, setProjects] = useState([]);
+    useEffect(() => {
+        api.get('/projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+    async function handleAddProject() {
+        
+        const response = await api.post('projects', {
+            title: `Novo Projeto: ${Date.now()}`,
+            owner: "Denis Carvalho"
+        });
 
+        console.log(response);
+        
+        const project = response.data;
+        setProjects([...projects, project]);
+    }
     return (
         <>
             <Header title="Projects">
 
             </Header>
+            <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
         </>
-        );
+    );
 }
 
 export default App;
