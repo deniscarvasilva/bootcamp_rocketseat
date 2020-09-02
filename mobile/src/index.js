a82a6b0def86437ec726b2b26f4894dc50fb4cda
deniscarvasilva <deniscarvasilva@gmail.com>
2020-09-01 23:58:39 -0300

    trabalhando com flatlist

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import api from './services/api';
 
 export default function App() {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        })
+    }, []);
     return (
-        <View style={styles.container}>
-            <Text>Hello Go Stack</Text>
-        </View>
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={projects}
+                    keyExtractor={project => project.id}
+                    renderItem={({ item }) => (
+                        <Text style={styles.project}>{item.title}</Text>
+                    )}
+                >
+
+                </FlatList>
+            </SafeAreaView>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-        backgroundColor: '#7159c1',
+        flex: 1,
+        backgroundColor: '#7159c1'
     },
+    project: {
+        color: '#fff',
+        fontSize: 20,
+    }
 })
